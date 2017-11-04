794cda0b6a9ca30b02f0a3644646fd1a25adf6fa
nayunhwan <nayunhwan.dev@gmail.com>
2017-11-05 00:03:42 +0900

    add Nickname

--- app.js
+++ app.js
@@ -1,31 +1,5 @@
 var video = document.getElementById('video');
-var canvas = document.getElementById('motion');
-var score = document.getElementById('score');
-var captured = document.getElementById('captured');
 var _canvas;
-
-var isLoading = false;
-
-$(function() {
-	$('button').click(function(){
-    analyzeImage();
-  });
-
-	setInterval(function() {
-		console.log('capture');
-		analyzeImage();
-	}, 10*1000);
-
-	navigator.getUserMedia({ video: {width: 640,	height: 480} },
-		function(localMediaStream) {
-			video.srcObject = localMediaStream;
-			console.log('test');
-		},
-		function(err) {
-			console.log(err);
-		}
-	);
-});
 
 function dataURLtoBlob(dataurl) {
     var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
@@ -47,12 +21,12 @@ function getBlob() {
 function analyzeImage() {
 	var url = 'https://alphaca.azurewebsites.net/api/HttpTriggerJS1?code=5LrKggzCpNWo9Lhf6gAaEpfetACN9dZtpZwDssockYzhi5VGWSKM1Q==';
 	var blob = getBlob();
-	var formData = new FormData();
 	$.ajax({
 		url: url,
 		type: 'POST',
 		headers: {
 			'_id': localStorage.getItem('_id'),
+      'nickname': $('#inputNickname').val(),
 			'Content-Type': "application/octet-stream",
 		},
 		processData: false,
@@ -65,3 +39,39 @@ function analyzeImage() {
 		}
 	});
 }
+
+$(function() {
+
+	// #2 '_id'라는 token을 생성합니다.
+	// 최초 접속 시간을 기준으로하여 hash화하며, hash 알고리즘은 SHA256을 사용합니다.
+	// 생성된 토큰은 localStorage의 '_id'값으로 저장하며, localStorage에 '_id' 토큰이 없는 경우에만 token을 생성하도록 합니다.
+	if (localStorage.getItem('_id') === null) {
+ 			var d = new Date();
+ 			localStorage.setItem('_id', SHA256(d.toString()));
+ 	}
+	console.log(localStorage.getItem('_id'));
+
+  // #3 웹 브라우저상에 웹캠 스트림을 표시합니다.
+  // navigator.getUserMedia(constraints, successCallback, errCallback) 메소드를 이용하여
+  // video 스트림을 받아와 <video/> 태그에 표시합니다.
+  navigator.getUserMedia({ video: true },
+		function(localMediaStream) {
+			video.srcObject = localMediaStream;
+		},
+		function(err) {
+			console.log(err);
+		}
+	);
+
+	// When the button is clicked, send the blob to the server.
+	$('button').click(function(){
+    analyzeImage();
+  });
+
+	// setInterval(function() {
+	// 	console.log('capture');
+	// 	analyzeImage();
+	// }, 10*1000);
+
+
+});
